0fd4b58c90beb4cd46241ed791d89fe3208a8b4f
Caleb Sabatini <calebsabatini@gmail.com>
2015-08-28 12:17:35 -0500

    moved from deprecated ActionBarActivity to AppCompActitvity base class

--- app/src/main/java/com/csab/soundboard/MainActivity.java
+++ app/src/main/java/com/csab/soundboard/MainActivity.java
@@ -16,7 +16,7 @@
 package com.csab.soundboard;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,7 +24,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private SoundPlayer mSoundPlayer;
 
